0c316f472d954718cd8ad0cf58f77b63c3f10dc0
Savannah Maselli <savannah.mas@lvjusd.k12.ca.us>
2018-12-03 13:04:05 -0800

    Happy Birthday

---
+++ Random Stuff/RandomStuff.java
@@ -0,0 +1,11 @@
+
+
+public class RandomStuff
+{
+    public static void main() {
+         for(int i=0; i< 400; i++) {
+        int age = (int)(Math.random()*8 +367);
+        System.out.println("The number is " + age + "!");
+    }
+}
+}
